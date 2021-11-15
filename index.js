7efead8ff8b6cb3806bb1795588113fcfc5bca6a
Adnane Drief <adnanedrief@gmail.com>
2021-11-15 23:41:09 +0100

    It is enough for today

--- index.js
+++ index.js
@@ -9,24 +9,48 @@ const TargetURL = "https://en.hespress.com/";
 
 app.listen(PORT, () => console.log("Server running on PORT : " + PORT));
 
+let Articles = [];
+let i = 1;
 async function ScrapeData() {
     try {
-        let numberOfDataNeeded = 1;
-        while (numberOfDataNeeded < 10) {
-            numberOfDataNeeded++;
-            let response = await cloudscraper.get(TargetURL + `?action=ajax_listing&paged=${i}`); // return String 
+        let numberOfPagesNeeded = 1;
+        // let Articles = [];
+        let Article = {
+            number: "",
+            date: "",
+            category: "",
+            title: "",
+            link: ""
+        };
+
+        while (numberOfPagesNeeded < 500) {
+            i++;
+            numberOfPagesNeeded++;
+            let response = await cloudscraper.get(TargetURL + `?action=ajax_listing&paged=${numberOfPagesNeeded}`); // return String 
             const $ = await cheerio.load(response);
             const prettyMe = pretty($.html()); // The html() method sets or returns the content of the selected elements.
 
             $('.cover', prettyMe).each(function() {
-                console.log($(this).find('.date-card').text());
-                console.log($(this).find('.cat').text()); // category
-                console.log($(this).find('.card-title').text());
-                console.log($(this).find('.card-img-top').find('a').attr('href'));
+                console.log(i); // to fix this counter later
+                i++;
+                Article.number = i;
+                Article.date = $(this).find('.date-card').text().trim();
+                Article.category = $(this).find('.cat').text().trim(); // category
+                Article.title = $(this).find('.card-title').text().trim();
+                Article.link = $(this).find('.card-img-top').find('a').attr('href');
+                Articles.push(Article);
             });
+            // console.clear(); // to add clear last line in console and not all console
+            console.log("Waiting...." + i);
+
+
         }
+        console.log(Articles);
     } catch (error) {
         console.log(error);
     }
+    app.get('/', function(req, res) {
+        res.send(Articles);
+    })
 }
 ScrapeData();
